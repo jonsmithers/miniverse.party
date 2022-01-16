cd0a5bd29c334bed7d06fab5a135331a93e0b98b
Jon Smithers <jon@smithers.dev>
2022-01-16 15:04:53 -0500

    Add scaffolding and whatnot

--- main.ts
+++ main.ts
@@ -1,10 +1,25 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello world!";
+const router: Router = new Router()
+  .get("/route", (context) => {
+    context.response.body = "Hello world from router";
+  })
+  .get("/websocket", (context) => {
+    const ws = context.upgrade();
+    console.log("ws", ws);
+  });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(async (context) => {
+  await send(context, context.request.url.pathname, {
+    root: `${Deno.cwd()}/public`,
+    index: "index.html",
+  });
 });
 
-console.log('listening at http://localhost:8000');
+console.log("listening at http://localhost:8000");
 await app.listen({ port: 8000 });
