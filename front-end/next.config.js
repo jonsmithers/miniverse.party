7eede092955b653a0e8986d5a016367b4f445f32
Jon Smithers <jon@smithers.dev>
2022-01-16 23:48:40 -0500

    Export nextjs assets to static files

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    // https://stackoverflow.com/questions/65487914/error-image-optimization-using-next-js-default-loader-is-not-compatible-with-n
+    loader: 'akamai',
+    path: '',
+  },
   reactStrictMode: true,
 }
 
